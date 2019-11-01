032efb43e2e8d6dcdefe70d3bdb7ff153f750615
T3k5upp0rt <kylan.childers@gmail.com>
2019-11-01 12:25:19 -0700

    Created article schema model

---
+++ models/article.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var MongoArticles = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+//    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  notes: [{
+     type: Schema.Types.ObjectId,
+     ref: "Note"
+  }]
+});
+
+var Article = mongoose.model("Article", MongoArticles);
+
+module.exports = Article;
